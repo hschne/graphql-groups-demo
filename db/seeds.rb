ec50ff9d2832ce7995fca186b4816a3fdcd49665
Hans-Jörg Schnedlitz <hans.schnedlitz@gmail.com>
2020-08-12 22:29:20 +0200

    More fixing the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ return if Movie.any?
 
 require 'csv'
 movies = CSV.read('db/movies.csv', headers: true).map(&:to_h)
-           .each { |attrs| attrs['release_date'] = Date.strptime(attrs['release_date'], '%m/%d/%y') }
+           .each { |attrs| attrs['release_date'] = Date.strptime(attrs['release_date'], '%m/%d/%Y') }
            .map { |attrs| Movie.new(attrs) }
 
 Movie.import(movies)
@@ -23,4 +23,4 @@ items = CSV.read('db/original.csv', headers: true).map(&:to_h)
   .map { |item| item.slice(*headers) }
   .each { |attrs| attrs['release_date'] = attrs['release_date'].gsub(%r/\/\d{2}$/, "/#{attrs['release_year']}") }
   .map { |attrs| attrs.except('release_year') }
-CSV.open('db/test.csv', 'wb', headers: items.first.keys, write_headers: true) { |csv| items.each { |x| csv << x } }
+CSV.open('db/movies.csv', 'wb', headers: items.first.keys, write_headers: true) { |csv| items.each { |x| csv << x } }
